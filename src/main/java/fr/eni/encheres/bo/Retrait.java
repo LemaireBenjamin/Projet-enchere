362a5a588616b5cdd404d29297510d021fa96512
Marie-Audrey SONZIA <marieaudrey.sonzia2023@campus-eni.fr>
2023-05-02 16:20:40 +0200

    initialisation BO

---
+++ src/main/java/fr/eni/encheres/bo/Retrait.java
@@ -0,0 +1,56 @@
+package fr.eni.encheres.bo;
+
+public class Retrait {
+	/**
+	 * Attributes
+	 */
+	
+	private String rue;
+	private String codePostal;
+	private String ville;
+	
+	//Association
+	private ArticleVendu articleVendu;
+	
+	/**
+	 * Consrtucteurs
+	 */
+	public Retrait () {}
+	
+	public Retrait(String rue, String codePostal, String ville) {
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+	//
+	public String getRue() {
+		return rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	//
+
+	@Override
+	public String toString() {
+		return "Retrait [rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + "]";
+	}
+
+}
