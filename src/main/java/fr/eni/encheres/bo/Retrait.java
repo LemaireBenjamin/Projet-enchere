61ffa92055ff723c879dd1c7e2dbb6fa1003318b
Marie-Audrey SONZIA <marieaudrey.sonzia2023@campus-eni.fr>
2023-05-03 10:40:20 +0200

    Conception Bo Fin

--- src/main/java/fr/eni/encheres/bo/Retrait.java
+++ src/main/java/fr/eni/encheres/bo/Retrait.java
@@ -4,25 +4,39 @@ public class Retrait {
 	/**
 	 * Attributes
 	 */
-	
+
 	private String rue;
 	private String codePostal;
 	private String ville;
-	
-	//Association
+
+	// Association
 	private ArticleVendu articleVendu;
-	
+
 	/**
 	 * Consrtucteurs
 	 */
-	public Retrait () {}
-	
+	public Retrait() {
+	}
+
 	public Retrait(String rue, String codePostal, String ville) {
 		this.rue = rue;
 		this.codePostal = codePostal;
 		this.ville = ville;
 	}
-	//
+
+	public Retrait(String rue, String codePostal, String ville, ArticleVendu articleVendu) {
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.articleVendu = articleVendu;
+	}
+
+	/**
+	 * Getter and setter
+	 * 
+	 * @return
+	 */
+
 	public String getRue() {
 		return rue;
 	}
@@ -33,6 +47,10 @@ public class Retrait {
 
 	public String getVille() {
 		return ville;
+	}
+
+	public ArticleVendu getArticleVendu() {
+		return articleVendu;
 	}
 
 	public void setRue(String rue) {
@@ -46,11 +64,19 @@ public class Retrait {
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
-	//
+
+	public void setArticleVendu(ArticleVendu articleVendu) {
+		this.articleVendu = articleVendu;
+	}
+
+	/**
+	 * ToString
+	 */
 
 	@Override
 	public String toString() {
-		return "Retrait [rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + "]";
+		return "Retrait [rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + ", articleVendu="
+				+ articleVendu + "]";
 	}
 
 }
